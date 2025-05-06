f42ac2a668723f27dc5f53c5db429a8d88c42c54
JoelW <waldmjoe@students.zhaw.ch>
2025-05-06 10:53:22 +0200

    third_dep

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('This is my second deployment!!!');
+  res.send('This is my third deployment!!!');
 });
 
 app.listen(PORT, HOST);
